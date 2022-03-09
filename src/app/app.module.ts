8198c9c9d41f8b98bf46259e7cc0c1cc57c377c1
kriss <kriss@ideta.io>
2022-03-09 10:18:50 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CvComponent } from './cv/cv.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CvComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/data/personne.ts
@@ -0,0 +1,18 @@
+let personne:any[]=[{
+  nom:"clotilde",
+  prenom:"kriss",
+  age:"30",
+  posteRecherche:"Developpeur FullStack",
+  langue:[{
+    nom:"français",niveau:"natale"
+  },{nom:"anglais",niveau:"avancé"}],
+  parcours:[{
+    entreprise:"ideta", dure:"1an",poste:"developpeur"
+  },{
+    entreprise:"Arkances", dure:"1an",poste:"developpeur"
+  },{
+    entreprise:"Audensiel", dure:"1mois",poste:"developpeur"
+  }]
+}]
+
+export default personne;
